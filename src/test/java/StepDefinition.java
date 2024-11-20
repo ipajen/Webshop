7086183dc8ba93e09378ad596c6c1a0776dc308f
Jarko Piironen <116973963+ipajen@users.noreply.github.com>
2024-11-20 16:24:56 +0100

    Updated Pom with downgrade version of org.seleniumhq.selenium to 4.24 to avoid warnings about 'net.bytebuddy.dynamic.loading.ClassInjector$UsingJna$Dispatcher$Windows32BitFunctionMappe
    Ticket:
    
    https://github.com/ipajen/Webshop/issues/3

--- src/test/java/StepDefinition.java
+++ src/test/java/StepDefinition.java
@@ -1,2 +1,49 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.java.BeforeAll;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StepDefinition {
+
+    private static WebDriver driver;
+    @BeforeAll
+    public static void createDriver()
+    {
+        driver = new ChromeDriver();
+        driver.get("https://webshop-agil-testautomatiserare.netlify.app/");
+    }
+
+    @Then("the copyright text should be {string}")
+    //Author: Jarko Piironen
+    public void theCopyrightTextShouldBe(String expectedCopyrightText) {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement websiteCopyright = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("  body > div:nth-child(3) > footer > p"))
+        );
+      
+        // Get the text from the copyright element
+        String websiteCopyrightText = websiteCopyright.getText();
+        System.out.println(" - Copyright text: " + websiteCopyrightText);
+
+        assertEquals(expectedCopyrightText, websiteCopyrightText);
+    }
+
+    @Given("Webshop is available")
+    public void webshopIsAvailable() {
+    }
+
+
+    @When("User visits webshop-agil-testautomatiserare.netlify.app")
+    public void userVisitsWebshopAgilTestautomatiserareNetlifyApp() {
+    }
 }
