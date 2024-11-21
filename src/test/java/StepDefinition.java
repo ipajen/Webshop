a5df6c786238e92fefef1ab0de3d909d9361faa8
BarnaliD <115993238+BarnaliD@users.noreply.github.com>
2024-11-21 17:00:03 +0100

    Code for the checking the the main heading text of the website

--- src/test/java/StepDefinition.java
+++ src/test/java/StepDefinition.java
@@ -1,98 +1,87 @@
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.cucumber.java.BeforeAll;
-import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import javax.net.ssl.HttpsURLConnection;
-import java.net.URI;
-import java.net.URL;
-import java.security.cert.X509Certificate;
 import java.time.Duration;
-import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 
 public class StepDefinition {
 
-    private static WebDriver driver;
-    @BeforeAll
-    public static void createDriver()
-    {
-        ChromeOptions option = new ChromeOptions();
-        option.addArguments("--remote-allow-origin=*");
-        option.addArguments("incognito");
-        option.addArguments("-start-maximized");
-        option.addArguments("--disable-infobars");
-        option.addArguments("--disable-blink-features=AutomationControlled");
-        option.addArguments("--headless");
+    private static WebDriver driver;  // Instance variable for WebDriver
+    private  static WebDriverWait wait;
 
-        driver = new ChromeDriver(option);
+    @Before
+    public  static void createDriver() {
+        // Setup ChromeDriver options
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origin=*");
+        options.addArguments("incognito");
+        options.addArguments("--start-maximized");
+        options.addArguments("--disable-infobars");
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        // Uncomment below for headless mode if required
+        // options.addArguments("--headless");
+
+        // Initialize WebDriver
+        driver = new ChromeDriver(options);
+
+        // Initialize WebDriverWait
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        // Navigate to the Webpage
         driver.get("https://webshop-agil-testautomatiserare.netlify.app/");
     }
 
-    //Verify that the website copyright text displays correctly
-    //Author: Jarko Piironen
-    @Then("the copyright text should be {string}")
-
-    public void theCopyrightTextShouldBe(String expectedCopyrightText) {
-
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        WebElement websiteCopyright = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(By.cssSelector("  body > div:nth-child(3) > footer > p"))
-        );
-      
-        // Get the text from the copyright element
-        String websiteCopyrightText = websiteCopyright.getText();
-        System.out.println(" - Copyright text: " + websiteCopyrightText);
-
-        assertEquals(expectedCopyrightText, websiteCopyrightText);
+    @After
+    public void tearDown() {
+        // Close the browser if the driver is not null
+        if (driver != null) {
+            driver.quit();
+        }
     }
+
+    // Check the main text on the webpage "This shop is all you need" exist
+    //    Author: Barnali Mohanty
 
     @Given("Webshop is available")
     public void webshopIsAvailable() {
     }
 
 
-    @When("User visits webshop-agil-testautomatiserare.netlify.app")
-    public void userVisitsWebshopAgilTestautomatiserareNetlifyApp() {
+    @When("User checks the main heading")
+    public void userChecksTheMainHeading() {
+        // Placeholder: Action will be verified in the next step
     }
 
-    //Validate SSL certificate
-    //Author: Jarko Piironen
-    @Then("the SSL certificate should be valid and not expiring in {int} days")
-    public void theSSLCertificateShouldBeValidAndNotExpiringInDays(int minDays) {
-        try {
-        String siteUrl = "https://webshop-agil-testautomatiserare.netlify.app";
-        URL url = new URL(siteUrl);
+    @Then("the main heading should be {string}")
+    public void theMainHeadingShouldBe(String expectedHeading) {
+        // Locate the element
+        WebElement heading = driver.findElement(By.cssSelector("h2.display-4.fw-bold.lh-1"));
 
-        // Get the SSL certificate and check its validity
+        // Get the text of the element
+        String actualHeading = heading.getText();
 
-        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
-        connection.connect();
+        // Print the heading text
+        System.out.println("The main heading text is: " + actualHeading);
 
-            // Retrieve the SSL certificate
-            X509Certificate cert = (X509Certificate) connection.getServerCertificates()[0];
-            Date expiryDate = cert.getNotAfter();
-            Date currentDate = new Date();
-            long daysRemaining = (expiryDate.getTime() - currentDate.getTime()) / (1000 * 60 * 60 * 24);
 
-            // Check if the certificate is valid and has more than 30 days before expiry
-        assertTrue(daysRemaining > minDays, "SSL Certificate is expiring in less than " + minDays + " days!");
-        System.out.println("SSL Certificate is valid and expires in " + daysRemaining + " days.");
-    } catch (Exception e) {
-        e.printStackTrace();
-        Assertions.fail("Failed to retrieve SSL certificate: " + e.getMessage());
-        }
+        // Assert the text matches the expected value
+        assertEquals(expectedHeading, actualHeading, "The main heading text does  match the expected value.");
     }
+
+
+
+
+
+
+
 }
 
