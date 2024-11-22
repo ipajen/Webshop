1b178983f500224e673b0c656be0d68e387c04e9
BarnaliD <115993238+BarnaliD@users.noreply.github.com>
2024-11-22 10:05:52 +0100

    Added new stepdefinition and new feature to this branch

--- src/test/java/RunCucumberTest.java
+++ src/test/java/RunCucumberTest.java
@@ -3,6 +3,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/resources/features")
+//@CucumberOptions(features = "src/test/java/resources/features")
+@CucumberOptions(     features = "src/test/resources/features",     glue = {"stepdefinitions"} )
 public class RunCucumberTest {
 }
