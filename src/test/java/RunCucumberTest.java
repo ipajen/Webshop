8f470260d577595ba91a526b1413328d0e81316c
Jarko Piironen <116973963+ipajen@users.noreply.github.com>
2024-11-22 10:26:33 +0100

    restored CucumberOptions

--- src/test/java/RunCucumberTest.java
+++ src/test/java/RunCucumberTest.java
@@ -3,7 +3,6 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-//@CucumberOptions(features = "src/test/java/resources/features")
-@CucumberOptions(     features = "src/test/resources/features"    )
+@CucumberOptions(features = "src/test/java/resources/features")
 public class RunCucumberTest {
 }
